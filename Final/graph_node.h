5eef2005217da3abeb46904cc714fa4b23d3a96c
BryceAH <131019945+BryceAH@users.noreply.github.com>
2024-06-06 19:53:51 -0700

    Create graph_node.h
    v1

---
+++ Final/graph_node.h
@@ -0,0 +1,36 @@
+//graph_node.h
+
+#pragma once
+
+
+#include <string>
+#include <vector>
+#include "edge.h"
+
+
+using namespace std;
+using std::vector;
+
+struct edge;
+
+class GraphNode
+    {
+
+    public:
+        GraphNode();
+
+        void addedge(GraphNode *destination, int weight);
+
+        string getValue();
+
+        vector<edge *> getNeighbor();
+        
+
+    private:
+
+        string value;
+
+        vector<edge *> neighbors;
+    };
+
+    
