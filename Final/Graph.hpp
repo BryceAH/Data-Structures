187b83144b8b677b4fc8f688ceec1441a0be45e2
BryceAH <131019945+BryceAH@users.noreply.github.com>
2024-06-06 21:41:52 -0700

    Update Graph.hpp
    v2

--- Final/Graph.hpp
+++ Final/Graph.hpp
@@ -1,5 +1,8 @@
 //Graph.hpp
 
+// starts at 60 in cs260_spring_2023 session 27 
+
+
 #include "graph_node.h"   
 
 class Graph
@@ -7,6 +10,22 @@ class Graph
 
 public:
 
+    Graph();
+    ~Graph();
+
+    void addNode(string newName);
+
+    void addEdge(string sourceName, string destinationName, int weight);
+
+    int getSize();
+
+    string shortestPath(string sourceName);
+
+    string minimumSpanningTree();
 private:
+
+    vector<GraphNode *> nodes;
+
+
 
 };
