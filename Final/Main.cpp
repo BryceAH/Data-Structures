31c44e7bfd124de175e6a252a837831f1c493272
BryceAH <131019945+BryceAH@users.noreply.github.com>
2024-06-06 21:40:56 -0700

    Update Main.cpp
    v2

--- Final/Main.cpp
+++ Final/Main.cpp
@@ -1,10 +1,11 @@
 //main.cpp
 
-//at 31:38 in cs260_spring_2023 session 27 
+//at 60 in cs260_spring_2023 session 27 
 
 #include <iostream>
 #include "graph_node.h"
 #include "edge.h"
+#include "Graph_node.cpp"
 
 using namespace std;
 
@@ -13,8 +14,17 @@ int main()
 edge *testEdge = new edge{nullptr, nullptr, 42};
 cout << "testEdge ->source: " << testEdge->source << "\ntestEdge->Destination: " << testEdge->destination << "\ntestEdge->weight: " << testEdge->weight << endl;
 
-GraphNode newNode; 
-// cout << "newNode.getValue: " << newNode.getValue() << "\nnewNode.getNeighbors().size(): "<< newNode.getNeighbor().size() << endl;
+GraphNode *newNode = new GraphNode("Albany"); 
+cout << "newNode->getValue: " << newNode->getValue() << "\nnewNode->getNeighbors().size(): "<< newNode->getNeighbor().size() << endl;
+
+newNode->addedge(newNode, 1);
+
+cout << "newNode->getNeighbor().size(): " << newNode->getNeighbor().size() << endl;
+cout << "newNode->getNeighbor().at(0)->destination->getValue(): " << newNode->getNeighbor().at(0)->destination->getValue() << endl;
+
+
+delete newNode;
+
 
 cout << "end of program" << endl;
 
