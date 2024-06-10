05d89fb4af06bb49b08c31bd841956ad57c5be00
BryceAH <131019945+BryceAH@users.noreply.github.com>
2024-06-09 19:13:10 -0700

    Update edge.h
    v6

--- Final/edge.h
+++ Final/edge.h
@@ -1,5 +1,3 @@
-// edge.h
-
 #pragma once
 
 #include "graph_node.h"
@@ -8,7 +6,7 @@ class GraphNode;
 
 struct edge
 {
-    GraphNode * source; // the node being looked at
+    GraphNode * source; // the node your coming from
     GraphNode * destination; // the node your going to
     int weight; // distance between the source and dest
 };
