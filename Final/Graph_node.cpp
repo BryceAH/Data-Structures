382fec2b31b6b75863305d9975191f1d29588469
BryceAH <131019945+BryceAH@users.noreply.github.com>
2024-06-10 16:10:15 -0700

    Update Graph_node.cpp
    Final Version

--- Final/Graph_node.cpp
+++ Final/Graph_node.cpp
@@ -1,3 +1,16 @@
+/*
+Graph_node.cpp
+Name: Bryce Hillsman
+Student id: x00505693
+Date: 6/10/2024
+
+    Graph_node.cpp contains all the functions for the GraphNode class such as GraphNode() which is the constructor that initializes the value
+to a provided name, the previous to null-pointer, and the distance to a very large number to simulate infinity. addedge() that adds the edges to the 
+graph. The getValue() which returns the value of the node. getNeighbor() which returns a vector of neighbors to the node. getDistance() which 
+returns the distance from the node to the source node. setDistance() which sets the distance from the node to the source node. setPrevious() which sets the 
+previous node in the path to the source node. getPrevious() which returns the previous node.
+*/
+
 #pragma once
 
 #include "graph_node.h"
